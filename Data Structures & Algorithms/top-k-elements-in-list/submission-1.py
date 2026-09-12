5ad8a0ee5c6a7256fc2abfe5bc3505bfca43f7a5
Thanapol Singjaroen <167920410+Thanapol1994@users.noreply.github.com>
2026-09-12 13:24:17 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # create a dictionary
+        numFrequency = {}
+
+        # count how many times each number occurs in the array and track it with the dictionary
+        for i in nums:
+            numFrequency[i] = numFrequency.get(i, 0) +1
+
+        # create a list of the numbers sorted by their frequency
+        sortedNumbers = sorted(numFrequency, key=numFrequency.get, reverse=True)
+        
+
+        # return a list of the k most frequnct elements
+        numTopK = sortedNumbers[:k]
+
+        return numTopK
